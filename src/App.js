25d082273da8aa57188bcada6c8f19f7cec0e39c
saikrishna <saik81067@gmail.com>
2023-05-13 08:42:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { Navbar, Brand, Cta } from './Components'
+import { Footer, Blog, Possibility, Features, Whatgpt3, Header } from './containers'
+import './App.css'
 function App() {
   return (
+   <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="gradient__bg">
+ 
+        <Navbar/>
+        <Header/>
+      </div>
+      <Brand/>
+      <Whatgpt3/>
+      <Features/>
+      <Possibility/>
+      <Cta/>
+      <Blog/>
+      <Footer/>
+      
     </div>
+    </>
   );
 }
 
---
+++ src/Components/brand/Brand.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './brand.css';
+import { google, slack, atlassian, dropbox, shopify } from './import';
+
+
+function Brand() {
+  return (
+    <div>
+       <div className="gpt3__brand section__padding">
+    <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
+    </div>
+  )
+}
+
+export default Brand
